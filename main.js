c97cc728fc06b23fa41568c8d5fcd251c39fe7bf
Grayson Newell <gln2109@columbia.edu>
2026-05-10 23:58:24 -0400

    Final Version

--- main.js
+++ main.js
@@ -3,6 +3,32 @@
 
 let audioCtx;
 let globalGain;
+let timer;
+let playing = false;
+let WHITE_NOISE;
+const S_GAIN = 0.0001
+
+const INPUT_MAP = {
+    "k": () => kick(),
+    "s": () => snare(),
+    "h": () => hat()
+}
+const REST_KEY = "x"
+
+let kick_tuning = {"name": "kick", "volume": 0.8, "attack": 0.01, "decay": 0.2,
+    "start_freq": 150, "end_freq": 40};
+
+let snare_tuning = {"name": "snare", "volume": 0.5, "attack": 0.02, "decay": 0.35,
+    "hp_filter_freq": 4000, "osc_freq": 150, "osc_gain_ratio": 0.6, "osc_decay_ratio": 0.5};
+
+let hat_tuning = {"name": "hat", "volume": 0.8, "attack": 0.02, "decay": 0.15,
+    "bp_filter_freq": 9000, "bp_filter_q": 3};
+
+const drum_tunings = [kick_tuning, snare_tuning, hat_tuning];
+const slider_scales = {"volume": 0.1, "attack": 0.01, "decay": 0.05, "start_freq": 10, "end_freq": 10,
+    "hp_filter_freq": 500, "osc_freq": 10, "osc_gain_ratio": 0.1, "osc_decay_ratio": 0.1,
+    "bp_filter_freq": 500, "bp_filter_q": 0.25};
+
 document.addEventListener("DOMContentLoaded", function(event) {
     audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     globalGain = audioCtx.createGain();
@@ -17,23 +43,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
             updateTuning();
         }
         slider.addEventListener('input', updateValue);
-        updateValue();
     });
 
+    loadDefaultTuning();
     WHITE_NOISE = whiteNoiseBuffer();
 })
-
-const INPUT_MAP = {
-    "k": () => kick(),
-    "s": () => snare(),
-    "h": () => hat(),
-    "c": () => clap()
-}
-const REST_KEY = "x"
-const S_GAIN = 0.0001
-let timer;
-let playing = false;
-let WHITE_NOISE;
 
 function parseSequence(unparsed) {
     let sp = unparsed.split(" ");
@@ -63,7 +77,7 @@ function startLoop(sequence, bpm) {
     function mainLoop() {
         if (playing) {
             const time = audioCtx.currentTime;
-            const keys = sequence[i % sequence.length]
+            const keys = sequence[i % sequence.length];
             keys.forEach(key => {
                 if (key !== REST_KEY) {
                     INPUT_MAP[key]?.(time);
@@ -76,13 +90,12 @@ function startLoop(sequence, bpm) {
     mainLoop();
 }
 
-
 function kick(time = audioCtx.currentTime) {
     let volume = kick_tuning["volume"];
     let attack = kick_tuning["attack"];
     let decay = kick_tuning["decay"];
-    let osc_start_freq = 150;
-    let osc_end_freq = 40;
+    let osc_start_freq = kick_tuning["start_freq"];
+    let osc_end_freq = kick_tuning["end_freq"];
 
     const osc = audioCtx.createOscillator();
     osc.frequency.value = osc_start_freq;
@@ -94,34 +107,20 @@ function kick(time = audioCtx.currentTime) {
     gain.gain.setValueAtTime(volume, time + attack)
     gain.gain.exponentialRampToValueAtTime(S_GAIN, time + decay);
 
-    //WIP
-    const noise = audioCtx.createBufferSource();
-    noise.buffer = WHITE_NOISE;
-    const noiseFilter = audioCtx.createBiquadFilter()
-    noiseFilter.type = "highpass";
-    noiseFilter.frequency.value = 3000;
-    const noiseGain = audioCtx.createGain();
-    noiseGain.gain.setValueAtTime(0.15, time);
-    noiseGain.gain.exponentialRampToValueAtTime(S_GAIN, time + 0.01);
-    noise.connect(noiseFilter).connect(noiseGain).connect(globalGain);
-    noise.start(time);
-    noise.stop(time+0.01);
-    //WIP
-
-
     osc.connect(gain).connect(globalGain);
 
     osc.start();
     osc.stop(time + decay);
 }
 
-
 function snare(time = audioCtx.currentTime) {
     let volume = snare_tuning["volume"];
     let attack = snare_tuning["attack"];
     let decay = snare_tuning["decay"];
-    let osc_gain_ratio = 0.7;
-    let osc_decay_ratio = 0.8;
+    let filter_freq = snare_tuning["hp_filter_freq"];
+    let osc_gain_ratio = snare_tuning["osc_gain_ratio"];
+    let osc_decay_ratio = snare_tuning["osc_decay_ratio"];
+    let osc_freq = snare_tuning["osc_freq"];
 
     let noise = audioCtx.createBufferSource();
     noise.buffer = WHITE_NOISE;
@@ -134,7 +133,7 @@ function snare(time = audioCtx.currentTime) {
 
     let osc = audioCtx.createOscillator();
     osc.type = "triangle";
-    osc.frequency.setValueAtTime(180, time);
+    osc.frequency.setValueAtTime(osc_freq, time);
 
     let oscGain = audioCtx.createGain();
     oscGain.gain.setValueAtTime(S_GAIN, time);
@@ -144,7 +143,7 @@ function snare(time = audioCtx.currentTime) {
 
     const hpFilter = audioCtx.createBiquadFilter();
     hpFilter.type = "highpass";
-    hpFilter.frequency.value = 5000;
+    hpFilter.frequency.value = filter_freq;
 
     noise.connect(hpFilter).connect(noiseGain).connect(globalGain);
     osc.connect(oscGain).connect(globalGain);
@@ -155,11 +154,12 @@ function snare(time = audioCtx.currentTime) {
     osc.stop(time + decay);
 }
 
-
 function hat(time = audioCtx.currentTime) {
     let volume = hat_tuning["volume"];
     let attack = hat_tuning["attack"];
     let decay = hat_tuning["decay"];
+    let filter_freq = hat_tuning["bp_filter_freq"];
+    let filter_q = hat_tuning["bp_filter_q"];
 
     let noise = audioCtx.createBufferSource();
     noise.buffer = WHITE_NOISE;
@@ -170,81 +170,28 @@ function hat(time = audioCtx.currentTime) {
     noiseGain.gain.setValueAtTime(volume, time + attack)
     noiseGain.gain.exponentialRampToValueAtTime(S_GAIN, time + decay);
 
-    //let osc = audioCtx.createOscillator();
-    //osc.type = "square";
-    //osc.frequency.setValueAtTime(300, time);
-
-    //let oscGain = audioCtx.createGain();
-    //oscGain.gain.setValueAtTime(S_GAIN, time);
-    //oscGain.gain.exponentialRampToValueAtTime(volume * osc_gain_ratio, time + attack)
-    //oscGain.gain.setValueAtTime(volume * osc_gain_ratio, time + attack)
-    //oscGain.gain.exponentialRampToValueAtTime(S_GAIN, time + decay * osc_decay_ratio);
-
     const bpFilter = audioCtx.createBiquadFilter();
     bpFilter.type = "bandpass";
-    bpFilter.frequency.value = 9000;
-    bpFilter.Q.value = 3;
+    bpFilter.frequency.value = filter_freq;
+    bpFilter.Q.value = filter_q;
 
     noise.connect(bpFilter).connect(noiseGain).connect(globalGain);
     noise.start(time);
     noise.stop(time + decay);
-
-    //osc.connect(oscGain).connect(globalGain);
-    //osc.start(time);
-    //osc.stop(time + decay);
 }
 
-function clap(time = audioCtx.currentTime) {
-    let volume = clap_tuning["volume"];
-    //let attack = clap_tuning["attack"];
-    let attack = 0.01;
-    let decay = clap_tuning["decay"];
-    let sep = 0.015;
-    const offsets = [0, sep, sep * 2, sep * 3];
-    
-    offsets.forEach(offset => {
-       const noise = audioCtx.createBufferSource();
-       noise.buffer = WHITE_NOISE;
-
-       const filter = audioCtx.createBiquadFilter();
-       filter.type = "bandpass";
-       filter.frequency.value = 300;
-       //filter.Q.value = 0.7;
-       const gain = audioCtx.createGain();
-
-       gain.gain.setValueAtTime(S_GAIN, time + offset);
-       gain.gain.exponentialRampToValueAtTime(volume, time + offset + attack);
-       gain.gain.setValueAtTime(volume, time + offset + attack);
-       gain.gain.exponentialRampToValueAtTime(S_GAIN, time + offset + 0.08);
-
-       noise.connect(filter).connect(gain).connect(globalGain);
-
-       noise.start(time + offset);
-       noise.stop(time + offset + 0.08);
+function loadDefaultTuning() {
+    drum_tunings.forEach(tuning => {
+        Object.keys(tuning).forEach(key => {
+            if(key !== "name") {
+                const slider = document.getElementById(`${tuning["name"]}_${key}`);
+                const display = slider.previousElementSibling;
+                slider.value = (tuning[key] / slider_scales[key]);
+                display.textContent = (slider.value * slider_scales[key]).toFixed(2) + " " + display.dataset.unit;
+            }
+        });
     });
 }
-
-
-function whiteNoiseBuffer() {
-    var bufferSize = audioCtx.sampleRate,
-    noiseBuffer = audioCtx.createBuffer(1, bufferSize, audioCtx.sampleRate),
-    output = noiseBuffer.getChannelData(0);
-    for (var i = 0; i < bufferSize; i++) {
-        output[i] = Math.random() * 2 - 1;
-    }
-    return noiseBuffer;
-}
-
-
-let kick_tuning = {"name": "kick", "volume": 0.8, "attack": 0.01, "decay": 0.2};
-let snare_tuning = {"name": "snare", "volume": 0.8, "attack": 0.01, "decay": 0.2};
-let hat_tuning = {"name": "hat", "volume": 0.8, "attack": 0.01, "decay": 0.2};
-let clap_tuning = {"name": "clap", "volume": 0.8, "attack": 0.01, "decay": 0.2};
-let slider_scales = {"volume": 0.1, "attack": 0.01, "decay": 0.05};
-
-let drum_tunings = [kick_tuning, snare_tuning, hat_tuning, clap_tuning];
-
-
 
 function updateTuning() {
     drum_tunings.forEach(tuning => {
@@ -256,4 +203,14 @@ function updateTuning() {
     })
     globalGain.gain.setValueAtTime(document.getElementById("masterVolume").value * slider_scales["volume"],
         audioCtx.currentTime);
+}
+
+function whiteNoiseBuffer() {
+    const bufferSize = audioCtx.sampleRate,
+    noiseBuffer = audioCtx.createBuffer(1, bufferSize, audioCtx.sampleRate),
+    output = noiseBuffer.getChannelData(0);
+    for (let i = 0; i < bufferSize; i++) {
+        output[i] = Math.random() * 2 - 1;
+    }
+    return noiseBuffer;
 }
